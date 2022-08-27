70c7b7a42ffaa54660a1577ba582874e1cb88933
antarpreet11 <singhantarpreet11@gmail.com>
2022-08-27 02:13:42 -0700

    Added foods component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { Fragment } from 'react'
 import Header from './components/Layout/Header.jsx'
+import Foods from './components/Foods/Foods'
+
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Foods />
+      </main>
     </Fragment>
   );
 }
